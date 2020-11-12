53818cf6914f6d184b15d283ea3e77001359e216
John Millner <millnerjohn@gmail.com>
2020-11-11 23:07:42 -0500

    prototype checkpoint of macd transformer

--- stock-conveyor.go
+++ stock-conveyor.go
@@ -4,6 +4,7 @@ import (
 	"github.com/johnmillner/robo-macd/internal/alpaca_wrapper"
 	coordinatorLib "github.com/johnmillner/robo-macd/internal/coordinator"
 	"github.com/johnmillner/robo-macd/internal/gatherers"
+	"github.com/johnmillner/robo-macd/internal/transformers"
 	"github.com/johnmillner/robo-macd/internal/utils"
 	"log"
 	"time"
@@ -13,23 +14,27 @@ func main() {
 	// setup coordinator and receive main's configurator
 	coordinator, _ := coordinatorLib.InitCoordinator(make(chan utils.Message, 100))
 
+	equityData := make(chan []gatherers.Equity, 100000)
+	macdData := make(chan []transformers.Macd, 100000)
 	//initialize the gatherer
-	gatherer := gatherers.Gatherer{}.StartUp(coordinator.NewMessenger(gatherers.GathererConfig{
-		EquityData: make(chan []gatherers.Equity, 100000),
+	_ = gatherers.Gatherer{}.StartUp(coordinator.NewMessenger(gatherers.GathererConfig{
+		EquityData: equityData,
 		Alpaca:     alpaca_wrapper.Alpaca{},
 		Symbols:    []string{"TSLA"},
 		Period:     time.Minute,
 		Limit:      200,
 	}))
 
-	// sleep for just a moment to let the gatherer initialize before shutting it down next block
-	time.Sleep(time.Second)
-
-	// tell the gatherer to stop as an example
-	gatherer.ShutDown()
+	_ = transformers.MacdTransformer{}.StartUp(coordinator.NewMessenger(transformers.MacdConfig{
+		EquityData: equityData,
+		MacdData:   macdData,
+		Twelve:     12,
+		TwentySix:  26,
+		Nine:       9,
+	}))
 
 	// read through the output of the gatherer as an example
-	for simpleData := range gatherer.GetMessenger().Get().(gatherers.GathererConfig).EquityData {
+	for simpleData := range macdData {
 		for _, equity := range simpleData {
 			log.Printf("%v", equity)
 		}
